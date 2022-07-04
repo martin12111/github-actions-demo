f95f5490c3cbed0649fe4857cdec76f426b6f210
martin12111 <95372348+martin12111@users.noreply.github.com>
2022-07-04 21:11:40 +0200

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -5,3 +5,8 @@ def test_add():
     assert 2 == add(1, 1)
     assert 4 == add(3, 1)
     assert 8 == add(3, 5)
+    
+def test_add_2():
+    assert 1 == add(0, 1)
+    assert 3 == add(2, 1)
+    assert 7 == add(2, 5)
